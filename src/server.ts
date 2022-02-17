43654a680855dd5eeb6076d11860321fde4a2d53
Anderson Fernandes <andersonfferreira13@hotmail.com>
2022-02-17 12:42:15 -0300

    Configuring the eslint and prettier

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,6 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
-});
+app.get("/", (request, response) => response.json({ message: "Hello World" }));
 
 app.listen(3333, () => console.log("Server is running!"));
