bf2995f8a0af2bef6f3e99d16dda1b363ec1fd93
Anderson Fernandes <anderson.fernandes96@outlook.com>
2022-07-05 23:12:36 -0300

    Get current localization

--- web/src/components/Map.tsx
+++ web/src/components/Map.tsx
@@ -1,18 +1,55 @@
+import { useEffect, useState } from 'react';
 import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
 
+import { fetchLocalMapBox } from '../utils/apiMapBox';
+
+interface Position {
+  latitude: number | undefined;
+  longitude: number | undefined;
+}
 
 export const Map: React.FC = () => {
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+
+  function setLocalization() {
+    navigator.geolocation.getCurrentPosition(position => {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    });
+  }
+
+  useEffect(() => {
+    setLocalization();
+    console.log(latitude);
+    console.log(longitude);
+  }, [setLocalization]);
+
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{width: '100vw', height: '100vh'}}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    latitude && longitude ? (
+      <MapContainer
+        center={[latitude, longitude]}
+        zoom={13} scrollWheelZoom={true} style={{ width: '100vw', height: '100vh' }}>
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${import.meta.env.VITE_ACCESS_TOKEN_MAP_BOX}`}
+        />
+        <Marker position={[latitude, longitude]}>
+          <Popup>
+            Latitude: {latitude} <br/>
+            Longitude: {longitude}
+          </Popup>
+        </Marker>
+
+        <Marker position={[-19.6020179,-43.2177258]}>
+          <Popup>
+            Latitude: {latitude} <br/>
+            Longitude: {longitude}
+          </Popup>
+        </Marker>
+        
+      </MapContainer>
+    ) : (
+      <div></div>
+    )
   )
 }
