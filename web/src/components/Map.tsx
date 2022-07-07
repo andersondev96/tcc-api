40b4283bc081a7b3d8560f99442ae6e7d1bba426
Anderson Fernandes <anderson.fernandes96@outlook.com>
2022-07-06 21:07:01 -0300

    Customizing the popup

--- web/src/components/Map.tsx
+++ web/src/components/Map.tsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from 'react';
 import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
 
+import { AiFillHeart, AiOutlineCalculator, AiOutlineHeart, AiOutlineMail, AiOutlineWhatsApp } from 'react-icons/ai';
 import { fetchLocalMapBox } from '../utils/apiMapBox';
 
-interface Position {
-  latitude: number | undefined;
-  longitude: number | undefined;
-}
+import { MdOutlineChatBubbleOutline } from 'react-icons/md';
+
+import CoffeeImg from '../assets/coffee.png';
+import { BiWorld } from 'react-icons/bi';
+
+import './styles.css';
+
 
 export const Map: React.FC = () => {
   const [latitude, setLatitude] = useState(0);
@@ -29,24 +33,58 @@ export const Map: React.FC = () => {
     latitude && longitude ? (
       <MapContainer
         center={[latitude, longitude]}
-        zoom={13} scrollWheelZoom={true} style={{ width: '100vw', height: '100vh' }}>
+        zoom={16} scrollWheelZoom={true} style={{ width: '100vw', height: '100vh' }}>
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${import.meta.env.VITE_ACCESS_TOKEN_MAP_BOX}`}
         />
         <Marker position={[latitude, longitude]}>
-          <Popup>
-            Latitude: {latitude} <br/>
-            Longitude: {longitude}
+          <Popup closeButton={false}>
+            <div className="leaflet-popup-container">
+              <div className="leaflet-popup-icon-like">
+                <AiFillHeart className="icon-heart" size={18} />
+              </div>
+              <div className="leaflet-popup-image-container">
+                <img src={CoffeeImg} alt="Coffee" className="image" />
+              </div>
+              <div className="leaflet-popup-description">
+                <h1>Singhtglass Coffee</h1>
+                <span>Cafeteria</span>
+              </div>
+              <div className="leaflet-social-icons">
+                <BiWorld size={16} />
+                <AiOutlineWhatsApp size={16} />
+                <AiOutlineMail size={16} />
+                <MdOutlineChatBubbleOutline size={16} />
+                <AiOutlineCalculator size={16} />
+              </div>
+            </div>
           </Popup>
         </Marker>
 
-        <Marker position={[-19.6020179,-43.2177258]}>
+        <Marker position={[-19.6020179, -43.2177258]}>
           <Popup>
-            Latitude: {latitude} <br/>
-            Longitude: {longitude}
+            <div className="leaflet-popup-container">
+              <div className="leaflet-popup-icon-like">
+                <AiFillHeart className="icon-heart" size={18} />
+              </div>
+              <div className="leaflet-popup-image-container">
+                <img src={CoffeeImg} alt="Coffee" className="image" />
+              </div>
+              <div className="leaflet-popup-description">
+                <h1>Singhtglass Coffee</h1>
+                <span>Cafeteria</span>
+              </div>
+              <div className="leaflet-social-icons">
+                <BiWorld size={16} />
+                <AiOutlineWhatsApp size={16} />
+                <AiOutlineMail size={16} />
+                <MdOutlineChatBubbleOutline size={16} />
+                <AiOutlineCalculator size={16} />
+              </div>
+            </div>
           </Popup>
         </Marker>
-        
+
       </MapContainer>
     ) : (
       <div></div>
