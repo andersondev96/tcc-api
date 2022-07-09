bcf7e68ae2d4836168504bfbbcf5e3b9ca21e81f
Anderson Fernandes <anderson.fernandes96@outlook.com>
2022-07-09 15:55:13 -0300

    Finishing business page

--- web/src/pages/client/Business/index.tsx
+++ web/src/pages/client/Business/index.tsx
@@ -2,12 +2,28 @@ import { Header } from "../../../components/Header";
 import { BusinessHeader } from './components/BusinessHeader';
 import { Paragraph } from "./components/Paragraph";
 
+import Coffee1 from '../../../assets/coffee-img1.jpg';
+import Coffee2 from '../../../assets/coffee-img2.jpg';
+import Coffee3 from '../../../assets/coffee-img3.jpg';
+import Coffee4 from '../../../assets/coffee-img4.jpg';
+import { Pictures } from "./components/Pictures";
+import { AiFillHeart } from "react-icons/ai";
+import { Assessments } from "./components/Assessments";
+import { AssessmentsForm } from "./components/AssessmentsForm";
+
 export const Business: React.FC = () => {
+  const images = [
+    { id: 1, image: Coffee1, description: "Image1" },
+    { id: 2, image: Coffee2, description: "Image2" },
+    { id: 3, image: Coffee3, description: "Image3" },
+    { id: 4, image: Coffee4, description: "Image4" },
+  ]
+
   return (
     <div className="flex flex-col">
       <Header />
       <BusinessHeader />
-      <div className="px-[6.25rem] py-[2.625rem] mobile:px-[3.125rem] mobile:py-[1.75rem]">
+      <div className="px-[6.25rem] py-[2.625rem] mobile:px-[1.125rem] mobile:py-[1.75rem]">
         <div className="flex flex-col gap-8 mobile:gap-4">
           <Paragraph
             title="Sobre o negócio"
@@ -48,11 +64,38 @@ export const Business: React.FC = () => {
           <Paragraph
             title="Telefone"
             text="+1 415-861-1313"
-        />
+          />
+
+          <div className="flex flex-col gap-2">
+            <span className="font-inter font-semibold text-gray-700">Fotos</span>
+            <div className="flex flex-row mobile:grid mobile:grid-cols-2 gap-[1.25rem]">
+              {images.map(img => (
+                <Pictures key={img.id} image={img.image} description={img.description} />
+              ))}
+            </div>
+          </div>
+          <div>
+            <button className="flex flex-row items-center justify-center gap-4 border  border-gray-900 w-[9.375rem] h-[2.5rem] rounded hover:opacity-80 transition-opacity">
+              <AiFillHeart size={24} color="#FF0000" />
+              <span className="font-inter font-light">Favoritar</span>
+            </button>
+          </div>
+
+          <div className="flex flex-col mt-[2.25rem]">
+            <div className="flex flex-col gap-[0.375rem]">
+              <span className="font-inter font-semibold">Avaliações</span>
+              <p className="font-inter font-light text-xs">4 comentários</p>
+            </div>
+            <Assessments text="O melhor café da cidade" stars={3} />
+            <Assessments text="Atendimento de qualidade, estão de parabéns" stars={4} />
+            <Assessments text="Gostei muito do atendimento" stars={5} />
+          </div>
+
+          <div className="flex flex-row mt-8">
+            <AssessmentsForm />
+          </div>
+
         </div>
-
-        
-
       </div>
     </div>
   )
