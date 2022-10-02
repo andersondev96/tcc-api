a4baacb8d8cd7645698665fc73b4593a9a81be8d
Anderson Fernandes <anderson.fernandes96@outlook.com>
2022-10-01 23:27:49 -0300

    Create user tests

--- src/modules/users/__tests__/CreateUserService.spec.ts
+++ src/modules/users/__tests__/CreateUserService.spec.ts
@@ -10,40 +10,38 @@ let usersRepository: UsersRepositoryFake;
 let fakeHashProvider: FakeHashProvider;
 
 describe("CreateUserService", () => {
-  beforeEach(() => {
-    usersRepository = new UsersRepositoryFake();
-    fakeHashProvider = new FakeHashProvider();
-    createUserService = new CreateUserService(
-      usersRepository,
-      fakeHashProvider
-    );
-  });
+    beforeEach(() => {
+        usersRepository = new UsersRepositoryFake();
+        fakeHashProvider = new FakeHashProvider();
+        createUserService = new CreateUserService(
+            usersRepository,
+            fakeHashProvider
+        );
+    });
 
-  it("Should be able to create a new user", async () => {
-    const userData: User = {
-      name: "John doe",
-      email: "john@example.com",
-      password: "123456",
-    };
+    it("Should be able to create a new user", async () => {
+        const userData: User = {
+            name: "John doe",
+            email: "john@example.com",
+            password: "123456",
+        };
 
-    const user = await createUserService.execute(userData);
+        const user = await createUserService.execute(userData);
 
-    console.log(user);
+        expect(user).toHaveProperty("id");
+    });
 
-    expect(user).toHaveProperty("id");
-  });
+    it("Should not be able to create an existing user", async () => {
+        const userData: User = {
+            name: "John doe",
+            email: "john@example.com",
+            password: "123456",
+        };
 
-  it("Should not be able to create an existing user", async () => {
-    const userData: User = {
-      name: "John doe",
-      email: "john@example.com",
-      password: "123456",
-    };
+        const user = await createUserService.execute(userData);
 
-    const user = await createUserService.execute(userData);
-
-    await expect(createUserService.execute(user)).rejects.toBeInstanceOf(
-      AppError
-    );
-  });
+        await expect(createUserService.execute(user)).rejects.toBeInstanceOf(
+            AppError
+        );
+    });
 });
