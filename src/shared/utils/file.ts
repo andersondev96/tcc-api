c8609d6a447d6ea28943f0c9b3ab6f3b6ed3711d
Anderson Fernandes <anderson.fernandes96@outlook.com>
2022-10-27 00:23:14 -0300

    Create file utils

---
+++ src/shared/utils/file.ts
@@ -0,0 +1,14 @@
+import fs from "fs";
+
+export const deleteFile = async (filename: string) => {
+
+    try {
+        await fs.promises.stat(filename);
+    } catch {
+        return;
+    }
+
+    await fs.promises.unlink(filename);
+
+
+}
