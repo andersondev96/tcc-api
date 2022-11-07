8f7c26f53c245ada53dc20fa75c43bcd7b1c089c
Anderson Fernandes <anderson.fernandes96@outlook.com>
2022-11-06 21:02:05 -0300

    Update config jest

--- jest.config.ts
+++ jest.config.ts
@@ -83,7 +83,7 @@ export default {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: "<rootDir>/src",
+    prefix: "<rootDir>/src/",
   }),
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
