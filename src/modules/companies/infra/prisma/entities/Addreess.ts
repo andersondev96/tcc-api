39233bd62c93f3172b1f332769c82098dbd9772b
Anderson Fernandes <anderson.fernandes96@outlook.com>
2022-11-13 12:06:25 -0300

    Create Address entity

---
+++ src/modules/companies/infra/prisma/entities/Addreess.ts
@@ -0,0 +1,23 @@
+export class Address {
+    id?: string;
+    cep: string;
+    street: string;
+    district: string;
+    number: string;
+    state: string;
+    city: string;
+    company_id: string;
+
+    constructor({ cep, street, district, number, state, city, company_id }: Address) {
+        return Object.assign(this, {
+            cep,
+            street,
+            district,
+            number,
+            state,
+            city,
+            company_id
+        });
+    };
+
+}
