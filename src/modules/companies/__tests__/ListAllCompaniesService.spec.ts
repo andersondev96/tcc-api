a9e403fd0f49ddd25a9f8f0eee78606f45963eb1
Anderson Fernandes <anderson.fernandes96@outlook.com>
2022-11-19 16:30:28 -0300

    Refactoring ListAllCompaniesService

--- src/modules/companies/__tests__/ListAllCompaniesService.spec.ts
+++ src/modules/companies/__tests__/ListAllCompaniesService.spec.ts
@@ -1,23 +1,16 @@
-import { FakeUsersRepository } from "@modules/users/repositories/Fakes/FakeUsersRepository";
 import { IUsersRepository } from "@modules/users/repositories/IUsersRepository";
-import { AppError } from "@shared/errors/AppError";
-import { FakeAddressesRepository } from "../repositories/fakes/FakeAddressesRepository";
-import { FakeCompaniesRepository } from "../repositories/fakes/FakeCompaniesRepository";
-import { FakeContactsRepository } from "../repositories/fakes/FakeContactsRepository";
-import { FakeImagesCompanyRepository } from "../repositories/fakes/FakeImagesCompanyRepository";
-import { FakeSchedulesRepository } from "../repositories/fakes/FakeSchedulesRepository";
-import { IAddressesRepository } from "../repositories/IAddressesRepository";
 import { ICompaniesRepository } from "../repositories/ICompaniesRepository"
 import { IContactsRepository } from "../repositories/IContactsRepository";
 import { IImagesCompanyRepository } from "../repositories/IImagesCompanyRepository";
-import { ISchedulesRepository } from "../repositories/ISchedulesRepository";
+import { FakeUsersRepository } from "@modules/users/repositories/Fakes/FakeUsersRepository";
+import { FakeCompaniesRepository } from "../repositories/fakes/FakeCompaniesRepository";
+import { FakeContactsRepository } from "../repositories/fakes/FakeContactsRepository";
+import { FakeImagesCompanyRepository } from "../repositories/fakes/FakeImagesCompanyRepository";
 import { ListAllCompaniesService } from "../services/ListAllCompaniesService";
 
 let fakeUserRepository: IUsersRepository;
 let fakeCompanyRepository: ICompaniesRepository;
 let fakeContactRepository: IContactsRepository;
-let fakeScheduleRepository: ISchedulesRepository;
-let fakeAddressRepository: IAddressesRepository;
 let fakeImagesCompanyRepository: IImagesCompanyRepository;
 let listAllCompaniesService: ListAllCompaniesService;
 
@@ -27,17 +20,10 @@ describe("ListAllCompaniesService", () => {
         fakeUserRepository = new FakeUsersRepository();
         fakeCompanyRepository = new FakeCompaniesRepository();
         fakeContactRepository = new FakeContactsRepository();
-        fakeScheduleRepository = new FakeSchedulesRepository();
-        fakeAddressRepository = new FakeAddressesRepository();
         fakeImagesCompanyRepository = new FakeImagesCompanyRepository();
         listAllCompaniesService = new ListAllCompaniesService(
-            fakeCompanyRepository,
-            fakeContactRepository,
-            fakeScheduleRepository,
-            fakeAddressRepository,
-            fakeImagesCompanyRepository,
+            fakeCompanyRepository
         );
-
     });
 
     it("Should be able to list all companies", async () => {
