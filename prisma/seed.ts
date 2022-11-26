bc39b686b0c389f570fcd890f9f7e74e266e4296
Anderson Fernandes <anderson.fernandes96@outlook.com>
2022-11-26 16:47:10 -0300

    Create seeders

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,20 +1,126 @@
+import { faker } from "@faker-js/faker";
 import { PrismaClient } from "@prisma/client";
-
-import { users } from "./seeders/users";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  for (const user of users) {
+
+  Array.from({ length: 10 }).map(async () => {
     await prisma.user.create({
-      data: user,
-    })
-  }
+      data: {
+        name: faker.name.firstName(),
+        email: faker.internet.email(),
+        password: faker.internet.password()
+      }
+    });
+  });
+
+  Array.from({ length: 5 }).map(async () => {
+    await prisma.entrepreneur.create({
+      data: {
+        user: {
+          create: {
+            name: faker.name.firstName(),
+            email: faker.internet.email(),
+            password: faker.internet.password(),
+            Company: {
+              create: {
+                name: faker.company.name(),
+                cnpj: faker.random.alpha(13),
+                category: faker.company.bsNoun(),
+                physical_localization: true,
+                description: faker.lorem.text(),
+                services: faker.helpers.arrayElements(),
+                contact: {
+                  create: {
+                    telephone: faker.phone.number(),
+                    whatsapp: faker.phone.number(),
+                    email: faker.internet.email(),
+                    website: faker.internet.url()
+                  }
+                },
+                Address: {
+                  create: {
+                    cep: faker.address.zipCode(),
+                    street: faker.address.street(),
+                    district: faker.address.secondaryAddress(),
+                    number: faker.datatype.number(),
+                    state: faker.address.stateAbbr(),
+                    city: faker.address.cityName()
+                  }
+                },
+                Schedule: {
+                  createMany: {
+                    data: [
+                      {
+                        day_of_week: faker.date.weekday(),
+                        opening_time: faker.random.numeric(),
+                        closing_time: faker.random.numeric(),
+                        lunch_time: faker.random.numeric()
+                      },
+                      {
+                        day_of_week: faker.date.weekday(),
+                        opening_time: faker.random.numeric(),
+                        closing_time: faker.random.numeric(),
+                        lunch_time: faker.random.numeric()
+                      },
+                      {
+                        day_of_week: faker.date.weekday(),
+                        opening_time: faker.random.numeric(),
+                        closing_time: faker.random.numeric(),
+                        lunch_time: faker.random.numeric()
+                      }
+                    ]
+                  }
+                },
+                Service: {
+                  createMany: {
+                    data: [
+                      {
+                        name: faker.commerce.product(),
+                        description: faker.commerce.productDescription(),
+                        price: Number(faker.commerce.price()),
+                        category: faker.commerce.department()
+                      },
+                      {
+                        name: faker.commerce.product(),
+                        description: faker.commerce.productDescription(),
+                        price: Number(faker.commerce.price()),
+                        category: faker.commerce.department()
+                      },
+                      {
+                        name: faker.commerce.product(),
+                        description: faker.commerce.productDescription(),
+                        price: Number(faker.commerce.price()),
+                        category: faker.commerce.department()
+                      },
+                      {
+                        name: faker.commerce.product(),
+                        description: faker.commerce.productDescription(),
+                        price: Number(faker.commerce.price()),
+                        category: faker.commerce.department()
+                      },
+                      {
+                        name: faker.commerce.product(),
+                        description: faker.commerce.productDescription(),
+                        price: Number(faker.commerce.price()),
+                        category: faker.commerce.department()
+                      }
+                    ]
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    });
+  });
 }
 
 main().catch(e => {
   console.log(e);
-  process.exit(1)
+  process.exit(1);
 }).finally(() => {
   prisma.$disconnect;
 });
