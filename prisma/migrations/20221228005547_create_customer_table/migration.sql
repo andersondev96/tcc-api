20c7c10066fa4011237078dff705fc050a4de3c7
Anderson Fernandes <anderson.fernandes96@outlook.com>
2022-12-27 22:07:30 -0300

    Create assessments company migrations

---
+++ prisma/migrations/20221228005547_create_customer_table/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "customers" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "telephone" TEXT,
+    "status" BOOLEAN,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "customers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "customers_user_id_key" ON "customers"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "customers" ADD CONSTRAINT "customers_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
