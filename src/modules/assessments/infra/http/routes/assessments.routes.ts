9a4476c7375e81c92aa8905aee04b99b5cb9c4de
Anderson Fernandes <anderson.fernandes96@outlook.com>
2022-12-30 21:25:07 -0300

    Create validation to assessments

--- src/modules/assessments/infra/http/routes/assessments.routes.ts
+++ src/modules/assessments/infra/http/routes/assessments.routes.ts
@@ -1,5 +1,7 @@
+import { celebrate } from "celebrate";
 import { Router } from "express";
 
+import { AssessmentValidator } from "@modules/assessments/validator/AssessmentValidator";
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
 import { CreateAssessmentsCompanyController } from "../controllers/CreateAssessmentsCompanyController";
@@ -12,8 +14,8 @@ const updateAssessmentsByCompanyController = new UpdateAssessmentsByCompanyContr
 
 const assessmentsRoute = Router();
 
-assessmentsRoute.post("/company/:company_id", ensureAuthenticated, createAssessmentsCompanyController.handle);
+assessmentsRoute.post("/company/:company_id", ensureAuthenticated, celebrate(AssessmentValidator), createAssessmentsCompanyController.handle);
 assessmentsRoute.get("/company/:company_id", ensureAuthenticated, findAssessmentsByCompanyController.handle);
-assessmentsRoute.put("/company/:assessment_id", ensureAuthenticated, updateAssessmentsByCompanyController.handle);
+assessmentsRoute.put("/company/:assessment_id", ensureAuthenticated, celebrate(AssessmentValidator), updateAssessmentsByCompanyController.handle);
 
 export default assessmentsRoute;
