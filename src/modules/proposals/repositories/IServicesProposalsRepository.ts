a7f477e4308572f94ab600da83e0d17c5931f068
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-01-04 22:10:01 -0300

    Create ServicesProposals Repository

---
+++ src/modules/proposals/repositories/IServicesProposalsRepository.ts
@@ -0,0 +1,13 @@
+import { ICreateServiceProposalDTO } from "../dtos/ICreateServiceProposalDTO";
+import { ServiceProposal } from "../infra/prisma/entities/ServiceProposal";
+
+export interface IServicesProposalsRepository {
+
+  create(data: ICreateServiceProposalDTO): Promise<ServiceProposal>;
+
+  listServicesByProposal(proposal_id: string): Promise<ServiceProposal[]>;
+
+  update(data: ICreateServiceProposalDTO): Promise<ServiceProposal>;
+
+  delete(id: string): Promise<void>;
+}
