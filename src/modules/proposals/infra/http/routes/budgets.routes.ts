860f9fb650f7d8c5dc22842a698d0c49e301ef2e
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-01-10 00:29:07 -0300

    Validate budget data

--- src/modules/proposals/infra/http/routes/budgets.routes.ts
+++ src/modules/proposals/infra/http/routes/budgets.routes.ts
@@ -1,6 +1,8 @@
 
+import { celebrate } from "celebrate";
 import { Router } from "express";
 
+import { BudgetValidator } from "@modules/proposals/validator/BudgetValidator";
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
 import { CreateBudgetController } from "../controllers/CreateBudgetController";
@@ -9,6 +11,6 @@ const budgetsRoutes = Router();
 
 const createBudgetController = new CreateBudgetController();
 
-budgetsRoutes.post("/:proposal_id", ensureAuthenticated, createBudgetController.handle);
+budgetsRoutes.post("/:proposal_id", ensureAuthenticated, celebrate(BudgetValidator), createBudgetController.handle);
 
 export default budgetsRoutes;
