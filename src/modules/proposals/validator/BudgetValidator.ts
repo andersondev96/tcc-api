58e63f4df33f8f9ba12f70cc27e5d362a4ef8b8e
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-03-04 21:50:35 -0300

    Define delivery_date with date

--- src/modules/proposals/validator/BudgetValidator.ts
+++ src/modules/proposals/validator/BudgetValidator.ts
@@ -5,8 +5,8 @@ import { customMessage } from "@shared/utils/errors";
 const BudgetValidator = {
   [Segments.BODY]: Joi.object().keys({
     description: Joi.string().required().messages(customMessage("description")),
-    amount: Joi.date().required().messages(customMessage("delivery_date")),
-    delivery_date: Joi.number().required().messages(customMessage("delivery_date")),
+    amount: Joi.number().required().messages(customMessage("delivery_date")),
+    delivery_date: Joi.date().required().messages(customMessage("delivery_date")),
     installments: Joi.number().required().messages(customMessage("installments"))
   })
 };
