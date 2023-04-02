6609fcbf845df8134879466035329d9b5f40b6bc
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-04-02 18:37:09 -0300

    Upload image company logo

--- src/modules/entrepreneurs/__tests__/UploadImageLogoCompanyEntrepreneurService.spec.ts
+++ src/modules/entrepreneurs/__tests__/UploadImageLogoCompanyEntrepreneurService.spec.ts
@@ -4,15 +4,17 @@ import { FakeCompaniesRepository } from "@modules/companies/repositories/fakes/F
 import { FakeContactsRepository } from "@modules/companies/repositories/fakes/FakeContactsRepository";
 import { ICompaniesRepository } from "@modules/companies/repositories/ICompaniesRepository";
 import { IContactsRepository } from "@modules/companies/repositories/IContactsRepository";
-import { FakeEntrepreneursRepository } from "@modules/entrepreneurs/repositories/Fakes/FakeEntrepreneursRepository";
-import { IEntrepreneursRepository } from "@modules/entrepreneurs/repositories/IEntrepreneursRepository";
 import { FakeUsersRepository } from "@modules/users/repositories/Fakes/FakeUsersRepository";
 import { IUsersRepository } from "@modules/users/repositories/IUsersRepository";
+import { FakeStorageProvider } from "@shared/container/providers/StorageProvider/fakes/FakerStorageProvider";
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/models/IStorageProvider";
 import { AppError } from "@shared/errors/AppError";
 
+import { FakeEntrepreneursRepository } from "../repositories/Fakes/FakeEntrepreneursRepository";
 import { FakeEntrepreneursSettingsRepository } from "../repositories/Fakes/FakeEntrepreneursSettingsRepository";
+import { IEntrepreneursRepository } from "../repositories/IEntrepreneursRepository";
 import { IEntrepreneursSettingsRepository } from "../repositories/IEntrepreneursSettingsRepository";
-import { ShowSettingEntrepreneurCompanyService } from "../services/ShowSettingEntrepreneurCompanyService";
+import { UploadImageLogoCompanyEntrepreneurService } from "../services/UploadImageLogoCompanyEntrepreneurService";
 
 
 let fakeUserRepository: IUsersRepository;
@@ -21,9 +23,11 @@ let fakeCategoryRepository: ICategoriesRepository;
 let fakeContactRepository: IContactsRepository;
 let fakeEntrepreneurRepository: IEntrepreneursRepository;
 let fakeEntrepreneurSettingsRepository: IEntrepreneursSettingsRepository;
-let showSettingEntrepreneurCompanyService: ShowSettingEntrepreneurCompanyService;
+let fakeStorageProvider: IStorageProvider;
+let uploadImageLogoCompanyEntrepreneurService: UploadImageLogoCompanyEntrepreneurService;
 
-describe("ShowSettingEntrepreneurCompanyService", () => {
+
+describe("UploadImageLogoCompanyEntrepreneurService", () => {
   beforeEach(() => {
     fakeUserRepository = new FakeUsersRepository();
     fakeCompanyRepository = new FakeCompaniesRepository();
@@ -31,18 +35,19 @@ describe("ShowSettingEntrepreneurCompanyService", () => {
     fakeContactRepository = new FakeContactsRepository();
     fakeEntrepreneurRepository = new FakeEntrepreneursRepository();
     fakeEntrepreneurSettingsRepository = new FakeEntrepreneursSettingsRepository();
-    showSettingEntrepreneurCompanyService = new ShowSettingEntrepreneurCompanyService(
+    fakeStorageProvider = new FakeStorageProvider();
+    uploadImageLogoCompanyEntrepreneurService = new UploadImageLogoCompanyEntrepreneurService(
       fakeEntrepreneurSettingsRepository,
       fakeEntrepreneurRepository,
-      fakeCompanyRepository
+      fakeStorageProvider
     );
   });
 
-  it("Should be able to show settings entrepreneur company", async () => {
+  it("Should be able to upload an image company", async () => {
     const user = await fakeUserRepository.create({
       name: "John Doe",
       email: "john@example.com",
-      password: "123456"
+      password: "12345678"
     });
 
     const contact = await fakeContactRepository.create({
@@ -72,7 +77,7 @@ describe("ShowSettingEntrepreneurCompanyService", () => {
       company_id: company.id
     });
 
-    const entrepreneurSettings = await fakeEntrepreneurSettingsRepository.create({
+    await fakeEntrepreneurSettingsRepository.create({
       entrepreneur_id: entrepreneur.id,
       highlight_services_quantity: 5,
       online_budget: true,
@@ -80,24 +85,56 @@ describe("ShowSettingEntrepreneurCompanyService", () => {
       email_notification: true
     });
 
-    const showSettings = await showSettingEntrepreneurCompanyService.execute(
-      company.id
+    await uploadImageLogoCompanyEntrepreneurService.execute(
+      entrepreneur.id,
+      "image_url.png"
     );
 
-    expect(showSettings).toEqual(entrepreneurSettings);
+    const entrepreneurSettings = await fakeEntrepreneurSettingsRepository.findByEntrepreneur(
+      entrepreneur.id
+    );
+
+    expect(entrepreneurSettings).toHaveProperty("company_logo");
   });
 
-  it("Should not be able to how settings entrepreneur company if company not found", async () => {
-    await expect(showSettingEntrepreneurCompanyService.execute(
-      "company-not-exist"
+  it("Should not be able if entrepreneur not found", async () => {
+
+    await expect(uploadImageLogoCompanyEntrepreneurService.execute(
+      "entrepreneur-not-exist",
+      "logo_image.png"
     )).rejects.toBeInstanceOf(AppError);
   });
 
-  it("Should not be able to how settings entrepreneur company if company not found", async () => {
+  it("Should not be able if company not found", async () => {
     const user = await fakeUserRepository.create({
       name: "John Doe",
       email: "john@example.com",
-      password: "123456"
+      password: "12345678"
+    });
+
+    const entrepreneur = await fakeEntrepreneurRepository.create({
+      user_id: user.id
+    });
+
+    await fakeEntrepreneurSettingsRepository.create({
+      entrepreneur_id: entrepreneur.id,
+      highlight_services_quantity: 5,
+      online_budget: true,
+      online_chat: false,
+      email_notification: true
+    });
+
+    await expect(uploadImageLogoCompanyEntrepreneurService.execute(
+      entrepreneur.id,
+      "logo_image.png"
+    )).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("Should be able to upload an image company if image already exits", async () => {
+    const user = await fakeUserRepository.create({
+      name: "John Doe",
+      email: "john@example.com",
+      password: "12345678"
     });
 
     const contact = await fakeContactRepository.create({
@@ -122,8 +159,29 @@ describe("ShowSettingEntrepreneurCompanyService", () => {
       user_id: user.id
     });
 
-    await expect(showSettingEntrepreneurCompanyService.execute(
-      company.id
-    )).rejects.toBeInstanceOf(AppError);
+    const entrepreneur = await fakeEntrepreneurRepository.create({
+      user_id: user.id,
+      company_id: company.id
+    });
+
+    await fakeEntrepreneurSettingsRepository.create({
+      entrepreneur_id: entrepreneur.id,
+      highlight_services_quantity: 5,
+      online_budget: true,
+      online_chat: false,
+      email_notification: true,
+      company_logo: "image_url.png"
+    });
+
+    await uploadImageLogoCompanyEntrepreneurService.execute(
+      entrepreneur.id,
+      "image_url2.png"
+    );
+
+    const entrepreneurSettings = await fakeEntrepreneurSettingsRepository.findByEntrepreneur(
+      entrepreneur.id
+    );
+
+    expect(entrepreneurSettings.company_logo).toEqual("image_url2.png");
   });
 });
--- src/modules/entrepreneurs/infra/http/routes/entrepreneurs.routes.ts
+++ src/modules/entrepreneurs/infra/http/routes/entrepreneurs.routes.ts
@@ -1,19 +1,32 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "@config/upload";
 import { UpdateEntrepreneursSettingsController } from "@modules/entrepreneurs/infra/http/controllers/UpdateEntrepreneursSettingsController";
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
+import { ensureEntrepreneur } from "@shared/infra/http/middlewares/ensureEntrepreneur";
 
 import { ShowSettingEntrepreneurCompanyController } from "../controllers/ShowSettingEntrepreneurCompanyController";
 import { ShowSettingEntrepreneurUserController } from "../controllers/ShowSettingEntrepreneurUserController";
+import { UploadImageLogoCompanyEntrepreneurController } from "../controllers/UploadImageLogoCompanyEntrepreneurController";
 const entrepreneursRoutes = Router();
-
 
 const updateEntrepreneursSettingsController = new UpdateEntrepreneursSettingsController();
 const showSettingEntrepreneurUserController = new ShowSettingEntrepreneurUserController();
 const showSettingEntrepreneurCompanyController = new ShowSettingEntrepreneurCompanyController();
+const uploadImageLogoCompanyEntrepreneurController = new UploadImageLogoCompanyEntrepreneurController();
+
+const uploadImage = multer(uploadConfig);
 
 entrepreneursRoutes.put("/:entrepreneur_id/settings", ensureAuthenticated, updateEntrepreneursSettingsController.handle);
 entrepreneursRoutes.get("/", ensureAuthenticated, showSettingEntrepreneurUserController.handle);
 entrepreneursRoutes.get("/:company_id", showSettingEntrepreneurCompanyController.handle);
+entrepreneursRoutes.patch(
+  "/upload-logo/:entrepreneur_id",
+  ensureAuthenticated,
+  ensureEntrepreneur,
+  uploadImage.single("company_logo"),
+  uploadImageLogoCompanyEntrepreneurController.handle
+);
 
 export default entrepreneursRoutes;
