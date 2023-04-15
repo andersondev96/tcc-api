c7c35502153be230ccf311d3082fe07a0044c9c5
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-04-15 12:27:51 -0300

    Update validate user

--- src/modules/users/validator/UserValidator.ts
+++ src/modules/users/validator/UserValidator.ts
@@ -7,7 +7,7 @@ const userValidator = {
   [Segments.BODY]: Joi.object().keys({
     name: Joi.string().required().messages(customMessage("name")),
     email: Joi.string().email().required().messages(customMessage("email")),
-    password: Joi.string().alphanum().min(8).required().messages(customMessage("password")),
+    password: Joi.string().min(8).required().messages(customMessage("password")),
     isEntrepreneur: Joi.boolean().messages(customMessage("entrepreneur"))
   })
 };
