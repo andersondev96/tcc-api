7824c04a01747fbc85a170362a1b2792d679c400
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-04-16 14:13:10 -0300

    Limit to highlight services

--- src/modules/services/services/CreateServiceService.ts
+++ src/modules/services/services/CreateServiceService.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from "tsyringe";
 
 import { ICategoriesRepository } from "@modules/categories/repositories/ICategoriesRepository";
 import { ICompaniesRepository } from "@modules/companies/repositories/ICompaniesRepository";
+import { IEntrepreneursSettingsRepository } from "@modules/entrepreneurs/repositories/IEntrepreneursSettingsRepository";
 import { AppError } from "@shared/errors/AppError";
 
 import { Service } from "../infra/prisma/entities/Service";
@@ -29,7 +30,10 @@ export class CreateServiceService {
     private categoryRepository: ICategoriesRepository,
 
     @inject("CompaniesRepository")
-    private companiesRepository: ICompaniesRepository
+    private companiesRepository: ICompaniesRepository,
+
+    @inject("EntrepreneursSettingsRepository")
+    private entrepreneurSettingsRepository: IEntrepreneursSettingsRepository
 
   ) { }
 
@@ -46,6 +50,22 @@ export class CreateServiceService {
 
     if (!company) {
       throw new AppError("Company not found");
+    }
+
+    const sevicesByCompany = await this.serviceRepository.listServicesByCompany(company.id);
+
+    const contHighlightsService = sevicesByCompany.reduce((acc, service) => {
+      if (service.highlight_service === true) {
+        return acc + 1;
+      } else {
+        return acc;
+      }
+    }, 0);
+
+    const settings = await this.entrepreneurSettingsRepository.findByCompany(company.id);
+
+    if (contHighlightsService >= settings.highlight_services_quantity) {
+      throw new AppError(`Maximun services in highlight is ${settings.highlight_services_quantity}`);
     }
 
     const findCategoryCompany = await this.categoryRepository.findCategoryById(company.category_id);
