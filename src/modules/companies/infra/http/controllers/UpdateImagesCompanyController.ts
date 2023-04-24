cc13ff48ba35861763ff083e568f6cd9893663e8
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-04-23 23:20:04 -0300

    Update Images Company

--- src/modules/companies/infra/http/controllers/UpdateImagesCompanyController.ts
+++ src/modules/companies/infra/http/controllers/UpdateImagesCompanyController.ts
@@ -7,13 +7,24 @@ export class UpdateImagesCompanyController {
 
   public async handle(request: Request, response: Response): Promise<Response> {
 
-    const { id } = request.params;
+    const { company_id } = request.params;
 
-    const company = request.file.filename;
+    const { images } = request.body;
+
+    const imageUpdates = images.map((image: { id: string, filename: string }) => (
+      {
+        id: image.id,
+        image_name: image.filename,
+        image_url: "http://localhost:3333/companies",
+        company_id
+      }));
 
     const updateImagesCompanyService = container.resolve(UpdateImagesCompanyService);
 
-    await updateImagesCompanyService.execute(id, company);
+    await updateImagesCompanyService.execute({
+      company_id,
+      images: imageUpdates
+    });
 
     return response.status(204).send();
   }
--- src/modules/companies/services/UpdateImagesCompanyService.ts
+++ src/modules/companies/services/UpdateImagesCompanyService.ts
@@ -3,12 +3,30 @@ import { inject, injectable } from "tsyringe";
 import { IStorageProvider } from "@shared/container/providers/StorageProvider/models/IStorageProvider";
 import { AppError } from "@shared/errors/AppError";
 
+import { ImageCompany } from "../infra/prisma/entities/ImageCompany";
+import { ICompaniesRepository } from "../repositories/ICompaniesRepository";
 import { IImagesCompanyRepository } from "../repositories/IImagesCompanyRepository";
+
+interface IImages {
+  id?: string;
+  title?: string;
+  image_name: string;
+  image_url: string;
+  company_id: string;
+
+}
+interface IRequest {
+  company_id: string;
+  images: IImages[];
+}
 
 @injectable()
 export class UpdateImagesCompanyService {
 
   constructor(
+    @inject("CompaniesRepository")
+    private companyRepository: ICompaniesRepository,
+
     @inject("ImagesCompanyRepository")
     private imageCompanyRepository: IImagesCompanyRepository,
 
@@ -16,23 +34,74 @@ export class UpdateImagesCompanyService {
     private storageProvider: IStorageProvider
   ) { }
 
-  public async execute(id: string, image_name: string): Promise<void> {
+  public async execute({ company_id, images }: IRequest): Promise<ImageCompany[]> {
 
-    const image = await this.imageCompanyRepository.findImageById(id);
+    const company = await this.companyRepository.findById(company_id);
 
-    if (!image) {
-      throw new AppError("Image not found");
+    if (!company) {
+      throw new AppError("Company not found");
     }
 
-    await this.storageProvider.delete(image.image_name, "companies");
+    const listImages = await this.imageCompanyRepository.findImagesByCompany(company.id);
 
-    await this.storageProvider.save(image_name, "companies");
+    const newImages: IImages[] = [];
+    const imagesWithoutId: IImages[] = [];
 
-    const result = await this.imageCompanyRepository.update({
-      id: image.id,
-      company_id: image.company_id,
-      image_name,
-      image_url: `http://localhost:3333/companies/${image_name}`
-    });
+    for (const image of images) {
+      if (!image.id) {
+        imagesWithoutId.push(image);
+        continue;
+      }
+
+      const oldImagesIndex = listImages.findIndex(img => img.id === image.id);
+
+      if (oldImagesIndex === -1) {
+        newImages.push(image);
+      } else {
+        const oldImage = listImages[oldImagesIndex];
+
+        if (oldImage.image_name !== image.image_name) {
+          newImages.push(image);
+        }
+
+        listImages.splice(oldImagesIndex, 1);
+      }
+    }
+
+    // Deleta as imagens que não existem mais
+    for (const image of listImages) {
+      await this.storageProvider.delete(image.image_name, "companies");
+      await this.imageCompanyRepository.delete(image.id);
+    }
+
+    // Insere ou atualiza imagens
+    const updatedImages: IImages[] = [];
+
+    if (imagesWithoutId.length > 0) {
+      for (const image of imagesWithoutId) {
+        await this.storageProvider.save(image.image_name, "companies");
+        const addNewImages = await this.imageCompanyRepository.create({
+          image_name: image.image_name,
+          image_url: `http://localhost:3333/compoany/${image.id}`,
+          company_id
+        });
+
+        updatedImages.push(addNewImages);
+      }
+    }
+
+    for (const image of newImages) {
+      const updatedImage = await this.imageCompanyRepository.update({
+        id: image.id,
+        image_name: image.image_name,
+        image_url: `http://localhost:3333/compoany/${image.id}`,
+        company_id
+      });
+
+      updatedImages.push(updatedImage);
+    }
+
+    return updatedImages;
+
   }
 }
