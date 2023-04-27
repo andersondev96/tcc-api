cf330c8f6fc07ad099d7b0880d351891d386a0f6
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-04-26 22:35:15 -0300

    Insert new imagens on updating

--- src/modules/companies/__tests__/UpdateImagesCompanyService.spec.ts
+++ src/modules/companies/__tests__/UpdateImagesCompanyService.spec.ts
@@ -69,8 +69,6 @@ describe("UpdateImagesCompanyService", () => {
       images.map(image => fakeImagesCompanyRepository.create(image))
     );
 
-    const imagesCompany = await fakeImagesCompanyRepository.findImagesByCompany(company.id);
-
     const imagesUpdated = ["image1updated.jpg", "image2updated.jpg", "image3updated.jpg"];
 
     const updateImages = await updateImageCompanyService.execute({
@@ -87,6 +85,130 @@ describe("UpdateImagesCompanyService", () => {
     expect(findImage3.image_url).toEqual("http://localhost:3333/company/image3updated.jpg");
   });
 
+  it("Should be able to remove images on company", async () => {
+
+    const category = await fakeCategoryRepository.create({
+      name: "Category Test"
+    });
+
+    const company = await fakeCompanyRepository.create({
+      name: "New Company",
+      cnpj: "12345",
+      category_id: category.id,
+      description: "Description Company",
+      services: ["Service 1", "Service 2"],
+      contact_id: "contact-id",
+      physical_localization: false,
+      user_id: "user-id"
+    });
+
+    const images = [
+      {
+        image_name: "image1.jpg",
+        image_url: "http://localhost:3333/company/image1.jpg",
+        company_id: company.id
+      },
+      {
+        image_name: "image2.jpg",
+        image_url: "http://localhost:3333/company/image2.jpg",
+        company_id: company.id
+      },
+      {
+        image_name: "image3.jpg",
+        image_url: "http://localhost:3333/company/image3.jpg",
+        company_id: company.id
+      },
+      {
+        image_name: "image4.jpg",
+        image_url: "http://localhost:3333/company/image3.jpg",
+        company_id: company.id
+      },
+      {
+        image_name: "image5.jpg",
+        image_url: "http://localhost:3333/company/image3.jpg",
+        company_id: company.id
+      },
+      {
+        image_name: "image6.jpg",
+        image_url: "http://localhost:3333/company/image3.jpg",
+        company_id: company.id
+      }
+    ];
+
+    await Promise.all(
+      images.map(image => fakeImagesCompanyRepository.create(image))
+    );
+
+    const imagesUpdated = ["image1updated.jpg", "image2updated.jpg", "image3updated.jpg"];
+
+    const updateImages = await updateImageCompanyService.execute({
+      company_id: company.id,
+      images: imagesUpdated
+    });
+
+    expect(updateImages.length).toEqual(3);
+  });
+
+  it("Should be able to add new images on company", async () => {
+
+    const category = await fakeCategoryRepository.create({
+      name: "Category Test"
+    });
+
+    const company = await fakeCompanyRepository.create({
+      name: "New Company",
+      cnpj: "12345",
+      category_id: category.id,
+      description: "Description Company",
+      services: ["Service 1", "Service 2"],
+      contact_id: "contact-id",
+      physical_localization: false,
+      user_id: "user-id"
+    });
+
+    const images = [
+      {
+        image_name: "image1.jpg",
+        image_url: "http://localhost:3333/company/image1.jpg",
+        company_id: company.id
+      },
+      {
+        image_name: "image2.jpg",
+        image_url: "http://localhost:3333/company/image2.jpg",
+        company_id: company.id
+      },
+      {
+        image_name: "image3.jpg",
+        image_url: "http://localhost:3333/company/image3.jpg",
+        company_id: company.id
+      },
+      {
+        image_name: "image4.jpg",
+        image_url: "http://localhost:3333/company/image3.jpg",
+        company_id: company.id
+      }
+    ];
+
+    await Promise.all(
+      images.map(image => fakeImagesCompanyRepository.create(image))
+    );
+
+    const imagesUpdated = [
+      "image1updated.jpg", "image2updated.jpg", "image3updated.jpg", "image4updated.jpg", "image5.jpg", "image6.jpg"
+    ];
+
+    const updateImages = await updateImageCompanyService.execute({
+      company_id: company.id,
+      images: imagesUpdated
+    });
+
+    expect(updateImages.length).toEqual(6);
+    const findImage5 = updateImages.find((image) => image.image_name === "image5.jpg");
+    const findImage6 = updateImages.find((image) => image.image_name === "image6.jpg");
+
+    expect(findImage5.image_url).toEqual("http://localhost:3333/company/image5.jpg");
+    expect(findImage6.image_url).toEqual("http://localhost:3333/company/image6.jpg");
+  });
 
   it("Should not be able to update images if company not found", async () => {
 
--- src/modules/companies/services/UpdateImagesCompanyService.ts
+++ src/modules/companies/services/UpdateImagesCompanyService.ts
@@ -43,32 +43,68 @@ export class UpdateImagesCompanyService {
 
     const listImages = await this.imageCompanyRepository.findImagesByCompany(company.id);
 
-    const convertImages = images.map((imageString, index) => ({
-      id: listImages[index].id,
-      image_name: imageString,
-      image_url: `http://localhost:3333/company/${imageString}`,
-      company_id: company.id,
+    // Remove imagens
+
+    const indexesToRemove = listImages.slice(images.length).map((image) => image.id);
+
+    await Promise.all(indexesToRemove.map(async (id) => {
+      const imageToRemove = await this.imageCompanyRepository.findImageById(id);
+      await this.imageCompanyRepository.delete(imageToRemove.id);
+      await this.storageProvider.delete(imageToRemove.image_name, "company");
     }));
+
+
+    const convertImages = images.map((imageString, index) => {
+      const existingIndex = listImages.findIndex((_, i) => i === index);
+      if (existingIndex === -1) {
+        return {
+          id: null,
+          image_name: imageString,
+          image_url: `http://localhost:3333/company/${imageString}`,
+          company_id: company.id
+        };
+      }
+      const existingImage = listImages[existingIndex];
+      return {
+        id: existingImage.id,
+        image_name: imageString,
+        image_url: `http://localhost:3333/company/${imageString}`,
+        company_id: company.id
+      };
+    });
 
     listImages.forEach(async (image) => {
       await this.storageProvider.delete(image.image_name, "company");
     });
 
+    // Atualiza imagens existentes e cria novas imagens
+
     const updatedImages = await Promise.all(convertImages.map(async (image) => {
 
-      const updateImage = await this.imageCompanyRepository.update({
-        id: image.id,
-        image_name: image.image_name,
-        image_url: image.image_url,
-        company_id: company.id,
-      });
+      if (image.id) {
+        const updateImage = await this.imageCompanyRepository.update({
+          id: image.id,
+          image_name: image.image_name,
+          image_url: image.image_url,
+          company_id: company.id,
+        });
 
-      await this.storageProvider.save(image.image_name, "company");
+        await this.storageProvider.save(image.image_name, "company");
 
-      return updateImage;
+        return updateImage;
+      } else {
+        const newImage = await this.imageCompanyRepository.create({
+          image_name: image.image_name,
+          image_url: image.image_url,
+          company_id: company.id
+        });
+
+        await this.storageProvider.save(image.image_name, "company");
+
+        return newImage;
+      }
     }));
 
     return updatedImages;
-
   }
 }
