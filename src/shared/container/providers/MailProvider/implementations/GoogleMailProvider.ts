ca302496ac89b6a316be37579c024de1e10857d5
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-05-11 21:48:32 -0300

    Send mail with google

---
+++ src/shared/container/providers/MailProvider/implementations/GoogleMailProvider.ts
@@ -0,0 +1,41 @@
+import fs from "fs";
+import Handlebars from "handlebars";
+import nodemailer, { Transporter } from "nodemailer";
+import { injectable } from "tsyringe";
+
+import { IMailProvider } from "../models/IMailProvider";
+
+@injectable()
+export class GoogleMailProvider implements IMailProvider {
+  private client: Transporter;
+
+  constructor() {
+    this.client = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: "administrador@startbusinesshome.dev",
+        pass: "kknzbitmcgnyjqop"
+      }
+    });
+  }
+  async sendMail(to: string, subject: string, variables: any, path: string): Promise<void> {
+    const templateFileContent = fs.readFileSync(path).toString("utf-8");
+
+    const templateParse = Handlebars.compile(templateFileContent);
+
+    const templateHTML = templateParse(variables);
+
+    const message = await this.client.sendMail({
+      to,
+      from: "Administrador <administrador@startbusinesshome.dev>",
+      subject,
+      html: templateHTML
+    });
+
+    console.log('Message sent: %s', message.messageId);
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
+  }
+}
