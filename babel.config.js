dbde36ef5d97b4952e41b268d1056b377f9aaaad
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-05-22 21:24:51 -0300

    Babel configuration

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   presets: [
-    ["babel/preset-env", { targets: { node: "current" }}],
+    ["@babel/preset-env", { targets: { node: "current" }}],
     "@babel/preset-typescript"
   ],
   plugins: [
@@ -16,7 +16,9 @@ module.exports = {
       }
     ],
     "babel-plugin-transform-typescript-metadata",
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    
     ["@babel/plugin-proposal-class-properties", { loose: true }]
-
-  ]
+  ],
+  ignore: [/\.d\.ts$/]
 };
